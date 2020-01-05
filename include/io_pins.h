82c37e95e5a4fc42d75724d26e658175e930b57b
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-01-05 14:30:06 +0100

    Add LORA module pins

--- include/io_pins.h
+++ include/io_pins.h
@@ -1,2 +1,9 @@
 #define PIN_SDS011_RX 2
 #define PIN_SDS011_TX 3
+
+// PIN definition for module
+#define PIN_LMIC_NSS  10
+#define PIN_LMIC_RST  A0
+#define PIN_LMIC_DIO0 5
+#define PIN_LMIC_DIO1 6
+#define PIN_LMIC_DIO2 7
