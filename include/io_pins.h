a985f6844d2e1efa0b6a90169360d4037958bc58
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-02-09 16:05:46 +0100

    Update LMIC Pins

--- include/io_pins.h
+++ include/io_pins.h
@@ -1,8 +1,8 @@
 // PIN definition for module
-#define PIN_LMIC_NSS  10
-#define PIN_LMIC_RST  A0
-#define PIN_LMIC_DIO0 5
-#define PIN_LMIC_DIO1 6
-#define PIN_LMIC_DIO2 7
+#define PIN_LMIC_NSS  14
+#define PIN_LMIC_RST  22
+#define PIN_LMIC_DIO0 13
+#define PIN_LMIC_DIO1 12
+#define PIN_LMIC_DIO2 21
 
 #define PIN_ENABLE_SD011 5
