a5bbcd01179980bf4dbd02eada09498c081641b7
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-02-11 16:20:29 +0100

    Change Pin for default SDA/SCL Pin

--- include/io_pins.h
+++ include/io_pins.h
@@ -1,9 +1,9 @@
 // PIN definition for module
 #define PIN_LMIC_NSS  14
-#define PIN_LMIC_RST  22
+#define PIN_LMIC_RST  26
 #define PIN_LMIC_DIO0 13
 #define PIN_LMIC_DIO1 12
-#define PIN_LMIC_DIO2 21
+#define PIN_LMIC_DIO2 25
 
 #define PIN_ENABLE_SD011 5
 #define PIN_VBAT 27
