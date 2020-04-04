45208918e0ed6995556cdfd5ce365a260aaca4d5
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-04-04 11:31:54 +0200

    calibrate readings

--- include/settings.h
+++ include/settings.h
@@ -1,6 +1,6 @@
 #define LORA_TX_INTERVAL 300
 #define LMIC_LORA_SF  DR_SF7 // LORA Data rate
 #define SDS011_SERIAL Serial2 // Pin 16/17
-#define REF_VCCRAW_ESP32 985 // VCCraw read from ESP32
-#define REF_U2_DIVIDER 895 // Voltage in mV at voltage divider
-#define REF_VBAT 3960 // Voltage in mV at battery
+#define REF_VCCRAW_ESP32 994 // VCCraw read from ESP32
+#define REF_U2_DIVIDER 899 // Voltage in mV at voltage divider
+#define REF_VBAT 3913 // Voltage in mV at battery
