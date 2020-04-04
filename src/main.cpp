fe5a79dd65b92763db8d4896727c0d2edd5e7fd3
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-04-04 08:30:28 +0200

    Disable Brownout Detector

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <particle.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
 void setup() {
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // Disable Brownout Detector
     Serial.begin(115200);
     Serial.println(F("Starting environment sensor ..."));
 
