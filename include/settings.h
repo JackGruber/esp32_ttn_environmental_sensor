3e1b15c4118a03da8cc2c807dbe835ff58a16a92
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-04-13 11:37:07 +0200

    Adjust vbat calculation vars

--- include/settings.h
+++ include/settings.h
@@ -1,6 +1,5 @@
 #define LORA_TX_INTERVAL 300
 #define LMIC_LORA_SF  DR_SF7 // LORA Data rate
 #define SDS011_SERIAL Serial2 // Pin 16/17
-#define REF_VCCRAW_ESP32 994 // VCCraw read from ESP32
-#define REF_U2_DIVIDER 899 // Voltage in mV at voltage divider
-#define REF_VBAT 3913 // Voltage in mV at battery
+#define REF_VCCRAW_ESP32 984 // VCCraw read from ESP32
+#define REF_VBAT 3955 // Voltage in mV at battery
