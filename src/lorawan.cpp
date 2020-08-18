c9b991a7a9747d7268b3be6961d00530396011ee
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-08-18 20:37:18 +0200

    For DeepSleep query os_queryTimeCriticalJobs

--- src/lorawan.cpp
+++ src/lorawan.cpp
@@ -219,7 +219,7 @@ void onEvent (ev_t ev) {
 
 void LoraWANDo(void)
 {
-    if(GO_DEEP_SLEEP == true)
+    if(GO_DEEP_SLEEP == true AND !os_queryTimeCriticalJobs(ms2osticksRound( (seconds*1000) - 1000 )))
     {
         Serial.println(F("Go to DeepSleep ..."));
         LoraWANSaveLMICToRTC();
