3b1c949ff051eb72788e9af198a66599ebbc1115
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-08-30 11:39:01 +0200

    VBat calibrated

--- include/settings.h
+++ include/settings.h
@@ -1,5 +1,5 @@
 #define LORA_TX_INTERVAL 300
 #define LMIC_LORA_SF  DR_SF7 // LORA Data rate
 #define SDS011_SERIAL Serial2 // Pin 16/17
-#define REF_VCCRAW_ESP32 984 // VCCraw read from ESP32
-#define REF_VBAT 3955 // Voltage in mV at battery
+#define REF_VCCRAW_ESP32 931 // VCCraw read from ESP32
+#define REF_VBAT 3496 // Voltage in mV at battery
