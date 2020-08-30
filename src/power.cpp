f34e44bb7709e75e67adaa6f772b8ee31cbca7d3
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-08-30 11:38:16 +0200

    Power off ADC in DeepSleep

--- src/power.cpp
+++ src/power.cpp
@@ -1,4 +1,5 @@
 #include <power.h>
+#include <driver/adc.h>
 
 void PowerDeepSleepTimer(int seconds)
 {
@@ -8,6 +9,8 @@ void PowerDeepSleepTimer(int seconds)
 
 void PowerLightSleepTimer(int seconds)
 {
+    adc_power_off();
+
     esp_sleep_enable_timer_wakeup(seconds * 1000000);
     esp_light_sleep_start();
 }
